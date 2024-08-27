152d42005c73ea4186b0d413147da144925f8f30
Acux-dev <juansup123@gmail.com>
2024-08-26 19:40:03 -0500

    Player Rotation
    the player faces to the direction its moving

--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField]
     private float _speed;
 
+    [SerializeField]
+    private float _rotationSpeed;
+
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
@@ -33,13 +36,30 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        SetPlayerVelocity();
+        RotateInDirectionOfInput();
+    }
+
+    private void SetPlayerVelocity()
+    {
         _smoothedMovementInput = Vector2.SmoothDamp(
-            _smoothedMovementInput,
-            _movementInput,
-            ref _movementInputSmoothVelocity,
-            0.1f);
+                    _smoothedMovementInput,
+                    _movementInput,
+                    ref _movementInputSmoothVelocity,
+                    0.1f);
 
         _rigidbody.velocity = _smoothedMovementInput * _speed;
+    }
+
+    private void RotateInDirectionOfInput()
+    {
+        if (_movementInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+            _rigidbody.MoveRotation(rotation);
+        }
     }
 
     private void OnMove(InputValue inputValue)
