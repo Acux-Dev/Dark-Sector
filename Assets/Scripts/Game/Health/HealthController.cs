c29fc18a2cddeac15f18b7c2f14c7b2141e83900
Acux-dev <juansup123@gmail.com>
2024-09-23 15:22:13 -0500

    Feat: Health Bar
    Added health bar of the current health of the player

--- Assets/Scripts/Game/Health/HealthController.cs
+++ Assets/Scripts/Game/Health/HealthController.cs
@@ -30,6 +30,8 @@ public class HealthController : MonoBehaviour
 
     public UnityEvent OnDamaged;
 
+    public UnityEvent OnHealthChanged;
+
     public void TakeDamage(float damageAmount)
     {
         if (_currentHealth == 0)
@@ -43,6 +45,8 @@ public class HealthController : MonoBehaviour
         }
 
         _currentHealth -= damageAmount;
+
+        OnHealthChanged.Invoke();
 
         if (_currentHealth < 0)
         {
@@ -67,6 +71,8 @@ public class HealthController : MonoBehaviour
         }
 
         _currentHealth += amountToAdd;
+
+        OnHealthChanged.Invoke();
 
         if (_currentHealth > _maximumHealth)
         {
