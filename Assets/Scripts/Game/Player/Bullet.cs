a3a6c57693600c824f99891bac5fe88acec4b595
Acux-dev <juansup123@gmail.com>
2024-09-25 21:08:52 -0500

    Feat: Score system

--- Assets/Scripts/Game/Player/Bullet.cs
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -8,7 +8,8 @@ public class Bullet : MonoBehaviour
     {
         if (collision.GetComponent<EnemyMovement>())
         {
-            Destroy(collision.gameObject);
+            HealthController healthController = collision.GetComponent<HealthController>();
+            healthController.TakeDamage(10);
             Destroy(gameObject);
         }
     }
